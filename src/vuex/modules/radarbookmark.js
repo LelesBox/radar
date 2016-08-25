e4fae92217ab88a43b98aa83764544be4cd73fe6
LelesBox <li_xiaoliang@foxmail.com>
2016-08-25 21:27:03 +0800

    '添加vuex/modules‘

---
+++ src/vuex/modules/radarbookmark.js
@@ -0,0 +1,121 @@
+const state = {
+  bookmarks: [
+    [
+      {
+        type: 'default',
+        img: '',
+        text: '1',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '2',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '3',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '4',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '0000',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '10000',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '20000',
+        description: ''
+      }
+    ],
+    [
+      {
+        type: 'default',
+        img: '',
+        text: '1',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '2',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '3',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '4',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '1111',
+        description: ''
+      }
+    ], [
+      {
+        type: 'default',
+        img: '',
+        text: '1',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '2',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '3',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '4',
+        description: ''
+      },
+      {
+        type: 'default',
+        img: '',
+        text: '2222',
+        description: ''
+      }
+    ]
+  ],
+  types: [
+    'favorite', 'FE', '163'
+  ]
+}
+
+const mutations = {}
+
+export default {
+  state,
+  mutations
+}
---
+++ src/vuex/modules/search.js
@@ -0,0 +1,27 @@
+const state = {
+  engines: [
+    {
+      name: 'Google',
+      img: require('../../assets/images/google.png')
+    },
+    {
+      name: 'BAIDU',
+      img: require('../../assets/images/google.png')
+    },
+    {
+      name: 'NPM',
+      img: require('../../assets/images/google.png')
+    }
+  ],
+  current: {
+    name: 'Google',
+    img: require('../../assets/images/google.png')
+  }
+}
+
+const mutations = {}
+
+export default {
+  state,
+  mutations
+}
